d89197e6cc625e36daa34cd0132c75677f73e68e
Allan Pichardo <allan.pichardo@gmail.com>
2019-10-02 20:06:28 -0400

    changing tether shader

--- Assets/Scripts/Companion.cs
+++ Assets/Scripts/Companion.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.AI;
-using UnityEngine.Experimental.UIElements;
 
 [RequireComponent(typeof(LineRenderer))]
 [RequireComponent(typeof(NavMeshAgent))]

f1b148bafb9755ffb9c6b1d3e47386cdb6137761
Martin-John Hearty <martinjohn.hearty@gmail.com>
2019-11-05 11:51:59 -0500

    set-up tutorial level to open door

---
+++ Assets/Scripts/TestTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTrigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        Debug.Log("unlock door");
+        Usable usable = other.gameObject.GetComponent<Usable>();
+        if (usable)
+        {
+            usable.Use();
+        }
+    }
+    
+
+}

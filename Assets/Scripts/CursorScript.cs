f6861614a9f8937901926ac7942ffb28b9b9a459
Martin-John Hearty <martinjohn.hearty@gmail.com>
2019-11-22 23:08:07 -0500

    stairs

---
+++ Assets/Scripts/CursorScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+    
+}
